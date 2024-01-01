cee979b55f13f4db115fb14cdde12263a258e6c9
callumeppz <callumapps991@gmail.com>
2024-01-01 23:28:18 +0000

    first commit

---
+++ lists.py
@@ -0,0 +1,11 @@
+arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+#outputs
+print (arr[2:6]) #['c', 'd', 'e', 'f']
+print (arr[:3]) #['a', 'b', 'c']
+print (arr[4:]) #['e', 'f', 'g']
+
+print (arr[-2]) #First element of the list
+print (arr[len(arr) - 1]) #Fourth element of a list
+print (arr[3]) #Last element of the list
+print (arr[0]) #Second to last element of the list
