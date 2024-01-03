62f3877b9bd6dd01d94728597524e499ad3ae912
callumeppz <callumapps991@gmail.com>
2024-01-03 00:32:17 +0000

    more practise

---
+++ Dictionaries.py
@@ -0,0 +1,10 @@
+population = {'Shanghai' : 17.8, 'Istanbul' : 13.3, 'Karachi' : 13.0, 'Mumbai' : 12.5} 
+animals = {'dogs': [20, 10, 15, 8, 32, 15], 'cats': [3,4,2,8,2,4], 'rabbits': [2, 3, 3], 'fish': [0.3, 0.5, 0.8, 0.3, 1]}
+print(animals)
+
+room_numbers = {
+    ('Freddie', 'Jen'): 403,
+    ('Ned', 'Keith'): 391,
+    ('Kristin', 'Jazzmyne'): 411,
+    ('Eugene', 'Zach'): 395
+}
